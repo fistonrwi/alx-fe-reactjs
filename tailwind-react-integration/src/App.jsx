ed727eefce177efd5c508bc610c89174eedcde2c
Fiston Rwibutso <fistonfinn12@gmail.com>
2024-09-05 20:22:54 +0200

    Add UserProfile component styled with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+<<<<<<< HEAD
+=======
+import UserProfile from './components/UserProfile';
+>>>>>>> 9894e7b (Add UserProfile component styled with Tailwind CSS)
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+<<<<<<< HEAD
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +33,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+=======
+       <div className="App">
+      <UserProfile />
+    </div>
+>>>>>>> 9894e7b (Add UserProfile component styled with Tailwind CSS)
     </>
   )
 }
