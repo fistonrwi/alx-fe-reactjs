effc0d342e217178ba66ee38c1f4c59425058823
Fiston Rwibutso <fistonfinn12@gmail.com>
2024-09-07 12:55:58 +0200

    Initial commit

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-<<<<<<< HEAD
-=======
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import UserProfile from './components/UserProfile';
->>>>>>> 9894e7b (Add UserProfile component styled with Tailwind CSS)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-<<<<<<< HEAD
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -33,13 +29,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-=======
-       <div className="App">
-      <UserProfile />
-    </div>
->>>>>>> 9894e7b (Add UserProfile component styled with Tailwind CSS)
+      <div className="App">
+        <UserProfile />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
