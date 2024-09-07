bd22fcc28d5af986c010cf864547930168f0e5ed
Fiston Rwibutso <fistonfinn12@gmail.com>
2024-09-07 13:40:06 +0200

    Initial commit

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RegistrationForm from './components/RegistrationForm';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+      <h1>User Registration</h1>
+      <RegistrationForm />
+    </div>
+    </>
+  )
+}
+
+export default App
